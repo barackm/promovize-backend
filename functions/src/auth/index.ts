793f5c9a4d3a4b0ddd82fdc042aff4f4ff043b5d
Barack Mukelenga <barackmukelenga100@gmail.com>
2023-06-11 01:17:59 +0200

    Signup users with google auth

--- functions/src/auth/index.ts
+++ functions/src/auth/index.ts
@@ -1,16 +1,52 @@
-import * as logger from "firebase-functions/logger";
-import { onRequest } from "firebase-functions/v2/https";
+import * as logger from 'firebase-functions/logger';
+import { onRequest } from 'firebase-functions/v2/https';
+import { firestore } from 'firebase-admin';
 
-
-// listen for user signup (manual)
-export const signinWithGoogle = onRequest(async (request, response) => {
+export default {
+  signinWithGoogle: onRequest(async (request: any, response: any) => {
     try {
-        // const { uid, displayName, email, photoURL } = request.body;
-        console.log("signinWithGoogle", request.body);
-        logger.info("signinWithGoogle", { structuredData: true });
-        response.send({ message: "Signup successful" });
+      const { uid, displayName, email, photoURL } = request.body;
 
+      const usersCollection = firestore().collection('users');
+      const userDoc = await usersCollection.doc(uid).get();
+
+      if (userDoc.exists) {
+        response.send({ message: 'User already exists' });
+        return response.status(200);
+      }
+
+      const userData = {
+        uid,
+        firstName: displayName.split(' ')[0],
+        lastName: displayName.split(' ')[1],
+        email,
+        photoURL,
+        companyName: '',
+        industryTags: [],
+        notificationPreferences: {
+          contentRecommendations: true,
+          accountUpdates: true,
+          analyticsReports: false,
+        },
+        contactInformation: {
+          phone: '',
+          address: '',
+        },
+        securitySettings: {
+          twoFactorAuth: false,
+          securityQuestions: false,
+        },
+        termsAgreement: true,
+        emailUnsubscribe: false,
+        emailVerified: true, // Because we're using Google Sign-In
+      };
+
+      await usersCollection.doc(uid).set(userData);
+      logger.info('New user document created', { structuredData: true });
+      response.send({ message: 'User created successfully', data: userData });
     } catch (error) {
-
+      logger.error('Error creating user:', error);
+      response.status(500).send({ error: 'An error occurred while creating the user' });
     }
-});
+  }),
+};
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,4 @@
-import { onRequest } from "firebase-functions/v2/https";
 import * as admin from "firebase-admin";
 
 admin.initializeApp();
-import { signinWithGoogle as signinWithGoogleFunc } from "./auth";
-
-
-export const signinWithGoogle = onRequest(signinWithGoogleFunc);
+export { default as auth } from "./auth";
