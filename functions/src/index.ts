243f77af4a8e95e78385c5381ecb51b362890bff
Barack Mukelenga <barackmukelenga100@gmail.com>
2023-06-11 13:29:36 +0200

    Add SMPT config

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,3 +1,9 @@
 import * as admin from 'firebase-admin';
+import * as functions from 'firebase-functions';
+
 admin.initializeApp();
 export { userCreated } from './auth';
+
+const smtpConfig = functions.config().smtp;
+
+console.log(smtpConfig, 'smtpConfig++++');
---
+++ functions/src/services/mail.ts
@@ -0,0 +1,39 @@
+import * as nodemailer from 'nodemailer';
+
+const transporter = nodemailer.createTransport({
+  host: '',
+  port: 587,
+  auth: {
+    user: '',
+    pass: '',
+  },
+});
+
+export const sendWelcomeEmail = async (email: string, displayName: string) => {};
+export const sendPasswordResetEmail = async (email: string, displayName: string) => {};
+export const sendPasswordChangedEmail = async (email: string, displayName: string) => {};
+// confirmEmail
+export const sendConfirmEmail = async (email: string, displayName: string) => {
+  const mailOptions = {
+    from: 'Promovize <support@promovize.app>',
+    to: email,
+    subject: 'Welcome to Promovize!',
+    html: `
+            <div>
+                <h1>Welcome to Promovize</h1>
+                <p>Hi ${displayName},</p>
+                <p>Thanks for signing up for Your App! We're excited to have you as an early user.</p>
+                <p>Before we get started, we need to confirm your email address. Click the link below to confirm your email address:</p>
+                <p><a href="https://your-app.com/confirm-email?email=${email}">Confirm Email Address</a></p>
+                <p>Thanks,</p>
+                <p>Promovize Team</p>
+            </div>
+        `,
+  };
+  try {
+    await transporter.sendMail(mailOptions);
+    console.log('Verification email sent successfully');
+  } catch (error) {
+    console.error('Error sending verification email:', error);
+  }
+};
