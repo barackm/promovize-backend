50ef6dfc17bb8a431a2699f6df829ce9ecad0c47
Barack Mukelenga <barackmukelenga100@gmail.com>
2023-06-11 02:23:28 +0200

    Configure the emulators to avoid sending requests to production

--- functions/src/auth/index.ts
+++ functions/src/auth/index.ts
@@ -38,7 +38,55 @@ export default {
         },
         termsAgreement: true,
         emailUnsubscribe: false,
-        emailVerified: true, // Because we're using Google Sign-In
+        emailVerified: true, // Because we're using Google Sign-In,
+        authMethod: 'email',
+      };
+
+      await usersCollection.doc(uid).set(userData);
+      logger.info('New user document created', { structuredData: true });
+      response.send({ message: 'User created successfully', data: userData });
+    } catch (error) {
+      logger.error('Error creating user:', error);
+      response.status(500).send({ error: 'An error occurred while creating the user' });
+    }
+  }),
+
+  signUpWithEmail: onRequest(async (request: any, response: any) => {
+    try {
+      const { uid, email } = request.body;
+
+      const usersCollection = firestore().collection('users');
+      const userDoc = await usersCollection.doc(uid).get();
+
+      if (userDoc.exists) {
+        response.send({ message: 'User already exists' });
+        return response.status(200);
+      }
+
+      const userData = {
+        uid,
+        firstName: '',
+        lastName: '',
+        email,
+        companyName: '',
+        industryTags: '',
+        notificationPreferences: {
+          contentRecommendations: true,
+          accountUpdates: true,
+          analyticsReports: false,
+        },
+        contactInformation: {
+          phone: '',
+          address: '',
+        },
+        securitySettings: {
+          twoFactorAuth: false,
+          securityQuestions: false,
+        },
+        termsAgreement: true,
+        emailUnsubscribe: false,
+        emailVerified: false,
+        authMethod: 'email',
       };
 
       await usersCollection.doc(uid).set(userData);
