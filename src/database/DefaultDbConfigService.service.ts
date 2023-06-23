e94cb685dd79a391568b0131a93853fe6ff643e6
Barack Mukelenga <barackmukelenga100@gmail.com>
2023-06-23 20:33:27 +0200

    Inject the config service into the db service

--- src/database/DefaultDbConfigService.service.ts
+++ src/database/DefaultDbConfigService.service.ts
@@ -2,16 +2,20 @@ import { join } from 'path';
 import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
 import { Injectable } from '@nestjs/common';
 import { User } from 'src/modules/users/entities/user.enitity';
+import { ConfigService } from '@nestjs/config';
 @Injectable()
 export default class DefaultDbConfigService implements TypeOrmOptionsFactory {
+  constructor(private configService: ConfigService) {}
+
   createTypeOrmOptions(): TypeOrmModuleOptions {
+    const dbConfig = this.configService.get('database');
     return {
-      type: process.env.DB_TYPE as any,
-      host: process.env.DB_HOST,
-      port: parseInt(<string>process.env.DB_PORT),
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
+      type: dbConfig.type as any,
+      host: dbConfig.host,
+      port: dbConfig.port,
+      username: dbConfig.user,
+      password: dbConfig.password,
+      database: dbConfig.name,
       synchronize: false,
       logging: ['error'],
       migrationsRun: process.env.RUN_MIGRATIONS === 'true',
